389a7d266228d9e2f218faa389ff6eb9ae82d764
Aikar <aikar@aikar.co>
2013-06-02 10:42:57 +1000

    Spigot Timings
    Adds performance tracking timings all around the Minecraft Server, and improves the usability of the /timings command
    
    Plugins can track their own timings with CustomTimingsHandler

--- src/main/java/org/bukkit/Server.java
+++ src/main/java/org/bukkit/Server.java
@@ -1018,6 +1018,10 @@ public interface Server extends PluginMessageRecipient {
     public class Spigot
     {
 
+        public org.bukkit.configuration.file.YamlConfiguration getConfig()
+        {
+            throw new UnsupportedOperationException( "Not supported yet." );
+        }
     }
 
     Spigot spigot();
