2213a2b491bee8d5287d04f896e7a9044fc64233
md_5 <md_5@live.com.au>
2013-06-02 15:20:49 +1000

    BungeeCord Support

--- src/main/java/org/bukkit/entity/Player.java
+++ src/main/java/org/bukkit/entity/Player.java
@@ -1442,6 +1442,16 @@ public interface Player extends HumanEntity, Conversable, CommandSender, Offline
     public class Spigot extends Entity.Spigot
     {
 
+        /**
+         * Gets the connection address of this player, regardless of whether it
+         * has been spoofed or not.
+         *
+         * @return the player's connection address
+         */
+        public InetSocketAddress getRawAddress()
+        {
+            throw new UnsupportedOperationException( "Not supported yet." );
+        }
     }
 
     @Override
