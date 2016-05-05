a5a29ef1a92ddfee009e9d6bae7074e88f0db91c
Aikar <aikar@aikar.co>
2016-05-04 23:55:48 -0400

    Add getI18NDisplayName API
    Gets the Display name as seen in the Client.
    Currently the server only supports the English language. To override this,
    You must replace the language file embedded in the server jar.

--- src/main/java/org/bukkit/inventory/ItemFactory.java
+++ src/main/java/org/bukkit/inventory/ItemFactory.java
@@ -153,5 +153,16 @@ public interface ItemFactory {
      */
     @NotNull
     ItemStack ensureServerConversions(@NotNull ItemStack item);
+
+    /**
+     * Gets the Display name as seen in the Client.
+     * Currently the server only supports the English language. To override this,
+     * You must replace the language file embedded in the server jar.
+     *
+     * @param item Item to return Display name of
+     * @return Display name of Item
+     */
+    @Nullable
+    String getI18NDisplayName(@Nullable ItemStack item);
     // Paper end
 }
