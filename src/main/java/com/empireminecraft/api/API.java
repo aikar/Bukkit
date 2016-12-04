3fe6f19adbb6863e1b1d0c691ceec0f713284b08
Aikar <aikar@aikar.co>
2016-12-03 22:27:48 -0500

    Debug API's

--- src/main/java/com/empireminecraft/api/API.java
+++ src/main/java/com/empireminecraft/api/API.java
@@ -23,9 +23,30 @@
 
 package com.empireminecraft.api;
 
+import org.apache.commons.lang.exception.ExceptionUtils;
 
 public abstract class API {
 
     public static EAPI_Entity entity;
     public static EAPI_Misc misc;
+
+    public static String stack() {
+        return ExceptionUtils.getFullStackTrace(new Throwable());
+    }
+
+    public static void exception(Throwable e) {
+        exception(null, e);
+    }
+
+    public static void exception(String msg, Throwable e) {
+        if (msg != null) {
+            System.err.println(msg);
+        }
+        if (e.getMessage() != null) {
+            System.err.println(e.getMessage());
+        }
+        for (String line : ExceptionUtils.getFullStackTrace(e).split("\n")) {
+            System.err.println(line);
+        }
+    }
 }
