6999e3db738d23639a998be1bc9947a8db813517
Minecrell <minecrell@minecrell.net>
2017-09-21 16:14:13 +0200

    Handle plugin prefixes in implementation logging configuration
    Currently, plugin prefixes are prepended to the log message in
    the PluginLogger before passing the message to the underlying
    logging framework. This is bad design because they need to be
    stripped manually when using custom appenders to log messages
    in a different format.
    
    Additionally, it makes integration of alternative logging APIs hard
    because all logging must go through the PluginLogger. Avoid using
    PluginLogger and create a regular logger using the plugin name.
    The implementation should handle plugin prefixes by displaying
    logger names when appropriate.

--- src/main/java/org/bukkit/plugin/java/JavaPlugin.java
+++ src/main/java/org/bukkit/plugin/java/JavaPlugin.java
@@ -50,7 +50,7 @@ public abstract class JavaPlugin extends PluginBase {
     private boolean naggable = true;
     private FileConfiguration newConfig = null;
     private File configFile = null;
-    private PluginLogger logger = null;
+    private Logger logger = null; // Paper - PluginLogger -> Logger
 
     public JavaPlugin() {
         final ClassLoader classLoader = this.getClass().getClassLoader();
@@ -277,7 +277,8 @@ public abstract class JavaPlugin extends PluginBase {
         this.dataFolder = dataFolder;
         this.classLoader = classLoader;
         this.configFile = new File(dataFolder, "config.yml");
-        this.logger = new PluginLogger(this);
+        // Paper - Handle plugin prefix in implementation
+        this.logger = Logger.getLogger(description.getPrefix() != null ? description.getPrefix() : description.getName());
     }
 
     /**
