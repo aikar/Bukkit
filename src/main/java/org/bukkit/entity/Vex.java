74e074189ce7054c3431babf3b8f9a00c2bc9ad5
Aikar <aikar@aikar.co>
2018-07-04 15:29:21 -0400

    Vex#getSummoner API
    Get's the Mob that summoned this Vex

--- src/main/java/org/bukkit/entity/Vex.java
+++ src/main/java/org/bukkit/entity/Vex.java
@@ -1,6 +1,18 @@
 package org.bukkit.entity;
 
+import com.destroystokyo.paper.entity.SentientNPC;
+
 /**
  * Represents a Vex.
  */
-public interface Vex extends Monster { }
+public interface Vex extends Monster {
+    /**
+     * @return What Entity (most likely an Evoker, but not guaranteed) summoned this Vex
+     * @deprecated Use {@link #getSummoner()}
+     */
+    @Deprecated
+    default SentientNPC getOwner() { return (SentientNPC) getSummoner(); } // Paper
+
+    Mob getSummoner(); // Paper
+
+}
