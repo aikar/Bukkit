0a916ea42ebb9b8bd2406910a89d3e44d504aff6
Aikar <aikar@aikar.co>
2018-08-10 22:08:34 -0400

    Make EnderDragon extend Mob

--- src/main/java/org/bukkit/entity/EnderDragon.java
+++ src/main/java/org/bukkit/entity/EnderDragon.java
@@ -8,7 +8,7 @@ import org.jetbrains.annotations.Nullable;
 /**
  * Represents an Ender Dragon
  */
-public interface EnderDragon extends ComplexLivingEntity, Boss {
+public interface EnderDragon extends ComplexLivingEntity, Boss, org.bukkit.entity.Mob { // Paper - add Mob
 
     /**
      * Represents a phase or action that an Ender Dragon can perform.
