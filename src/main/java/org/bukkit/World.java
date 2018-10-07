70cc801cd9bf17d2c57e10b7a58de92c727c1f50
BillyGalbreath <Blake.Galbreath@GMail.com>
2018-10-07 00:54:15 -0500

    Add sun related API

--- src/main/java/org/bukkit/World.java
+++ src/main/java/org/bukkit/World.java
@@ -1780,6 +1780,16 @@ public interface World extends PluginMessageRecipient, Metadatable {
      */
     public void setFullTime(long time);
 
+    // Paper start
+
+    /**
+     * Check if it is currently daytime in this world
+     *
+     * @return True if it is daytime
+     */
+    public boolean isDayTime();
+    // Paper end
+
     /**
      * Returns whether the world has an ongoing storm.
      *
