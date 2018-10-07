753cb4675cb9e00799add5447aa03af4e1e32826
BillyGalbreath <Blake.Galbreath@GMail.com>
2018-10-07 04:29:51 -0500

    Add more Zombie API

--- src/main/java/org/bukkit/entity/Zombie.java
+++ src/main/java/org/bukkit/entity/Zombie.java
@@ -90,4 +90,55 @@ public interface Zombie extends Monster, Ageable {
      * @param time new conversion time
      */
     void setConversionTime(int time);
+    // Paper start
+    /**
+     * Check if zombie is drowning
+     *
+     * @return True if zombie conversion process has begun
+     */
+    boolean isDrowning();
+
+    /**
+     * Make zombie start drowning
+     *
+     * @param drownedConversionTime Amount of time until zombie converts from drowning
+     *
+     * @deprecated See {@link #setConversionTime(int)}
+     */
+    @Deprecated
+    void startDrowning(int drownedConversionTime);
+
+    /**
+     * Stop a zombie from starting the drowning conversion process
+     */
+    void stopDrowning();
+
+    /**
+     * Set if zombie has its arms raised
+     *
+     * @param raised True to raise arms
+     */
+    void setArmsRaised(boolean raised);
+
+    /**
+     * Check if zombie has arms raised
+     *
+     * @return True if arms are raised
+     */
+    boolean isArmsRaised();
+
+    /**
+     * Check if this zombie will burn in the sunlight
+     *
+     * @return True if zombie will burn in sunlight
+     */
+    boolean shouldBurnInDay();
+
+    /**
+     * Set if this zombie should burn in the sunlight
+     *
+     * @param shouldBurnInDay True to burn in sunlight
+     */
+    void setShouldBurnInDay(boolean shouldBurnInDay);
+    // Paper end
 }
