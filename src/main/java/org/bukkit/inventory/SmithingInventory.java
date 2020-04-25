c114da37593aadb09675d648433e1823246b7572
KennyTV <jahnke.nassim@gmail.com>
2020-04-25 23:31:28 +0200

    Add item slot convenience methods

--- src/main/java/org/bukkit/inventory/SmithingInventory.java
+++ src/main/java/org/bukkit/inventory/SmithingInventory.java
@@ -1,6 +1,68 @@
 package org.bukkit.inventory;
 
+import org.jetbrains.annotations.Nullable; // Paper
+
 /**
  * Interface to the inventory of a Smithing table.
  */
-public interface SmithingInventory extends Inventory { }
+public interface SmithingInventory extends Inventory {
+
+    // Paper start
+    /**
+     * Gets the input equipment (first slot).
+     *
+     * @return input equipment item
+     */
+    @Nullable
+    default ItemStack getInputEquipment() {
+        return getItem(0);
+    }
+
+    /**
+     * Sets the input equipment (first slot).
+     *
+     * @param itemStack item to set
+     */
+    default void setInputEquipment(@Nullable ItemStack itemStack) {
+        setItem(0, itemStack);
+    }
+
+    /**
+     * Gets the input mineral (second slot).
+     *
+     * @return input mineral item
+     */
+    @Nullable
+    default ItemStack getInputMineral() {
+        return getItem(1);
+    }
+
+    /**
+     * Sets the input mineral (second slot).
+     *
+     * @param itemStack item to set
+     */
+    default void setInputMineral(@Nullable ItemStack itemStack) {
+        setItem(1, itemStack);
+    }
+
+    /**
+     * Gets the result item.
+     *
+     * @return result
+     */
+    @Nullable
+    default ItemStack getResult() {
+        return getItem(2);
+    }
+
+    /**
+     * Sets the result item.
+     *
+     * @param itemStack item to set
+     */
+    default void setResult(@Nullable ItemStack itemStack) {
+        setItem(2, itemStack);
+    }
+    // Paper end
+}
