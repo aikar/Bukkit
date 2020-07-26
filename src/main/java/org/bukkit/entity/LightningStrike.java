28f140bd2dea074c7e10a6cbac4ab50275c271ba
KennyTV <jahnke.nassim@gmail.com>
2020-07-26 14:44:16 +0200

    More lightning API

--- src/main/java/org/bukkit/entity/LightningStrike.java
+++ src/main/java/org/bukkit/entity/LightningStrike.java
@@ -31,4 +31,38 @@ public interface LightningStrike extends Entity {
     @Override
     Spigot spigot();
     // Spigot end
+
+    // Paper start
+    /**
+     * Returns the amount of flash iterations that will be done before the lightning dies.
+     *
+     * @see #getLifeTicks() for how long the current flash will last
+     * @return amount of flashes that will be shown before the lightning dies
+     */
+    int getFlashCount();
+
+    /**
+     * Sets the amount of life iterations that will be done before the lightning dies.
+     * Default number of flashes on creation is between 1-3.
+     *
+     * @param flashes amount of iterations that will be done before the lightning dies, must to be a positive number
+     */
+    void setFlashCount(int flashes);
+
+    /**
+     * Returns the amount of ticks the current flash will do damage for.
+     * Starts with 2 by default, will damage while it is equal to or above 0, with the next flash beginning somewhere between 0 and -9.
+     *
+     * @return ticks the current flash will do damage for
+     */
+    int getLifeTicks();
+
+    /**
+     * Sets the amount of ticks the current flash will do damage/fire for.
+     * Default is 2 for each flash, on which the sound and effect will also be played.
+     *
+     * @param lifeTicks ticks the current flash will do damage for
+     */
+    void setLifeTicks(int lifeTicks);
+    // Paper end
 }
