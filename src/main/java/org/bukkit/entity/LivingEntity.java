ba819d4509fa4ad66547ca5e5bdac3a6da6a59c2
William Blake Galbreath <blake.galbreath@gmail.com>
2020-08-23 19:36:08 +0200

    Add playPickupItemAnimation to LivingEntity

--- src/main/java/org/bukkit/entity/LivingEntity.java
+++ src/main/java/org/bukkit/entity/LivingEntity.java
@@ -805,5 +805,28 @@ public interface LivingEntity extends Attributable, Damageable, ProjectileSource
      * @param jumping entity jump state
      */
     void setJumping(boolean jumping);
+
+    /**
+     * Plays pickup item animation towards this entity.
+     * <p>
+     * <b>This will remove the item on the client.</b>
+     * <p>
+     * Quantity is inferred to be that of the {@link Item}.
+     *
+     * @param item item to pickup
+     */
+    default void playPickupItemAnimation(@NotNull Item item) {
+        playPickupItemAnimation(item, item.getItemStack().getAmount());
+    }
+
+    /**
+     * Plays pickup item animation towards this entity.
+     * <p>
+     * <b>This will remove the item on the client.</b>
+     *
+     * @param item item to pickup
+     * @param quantity quantity of item
+     */
+    void playPickupItemAnimation(@NotNull Item item, int quantity);
     // Paper end
 }
