7431e73d926179f1839e5069f720153ebad64c6b
Mariell Hoversholm <proximyst@proximyst.com>
2020-11-14 16:19:58 +0100

    Add API for quit reason

--- src/main/java/org/bukkit/event/player/PlayerQuitEvent.java
+++ src/main/java/org/bukkit/event/player/PlayerQuitEvent.java
@@ -10,10 +10,17 @@ import org.jetbrains.annotations.Nullable;
  */
 public class PlayerQuitEvent extends PlayerEvent {
     private static final HandlerList handlers = new HandlerList();
+    private final QuitReason reason; // Paper
     private String quitMessage;
 
     public PlayerQuitEvent(@NotNull final Player who, @Nullable final String quitMessage) {
+        // Paper start
+        this(who, quitMessage, null);
+    }
+    public PlayerQuitEvent(@NotNull final Player who, @Nullable final String quitMessage, @Nullable QuitReason quitReason) {
         super(who);
+        this.reason = quitReason == null ? QuitReason.DISCONNECTED : quitReason;
+        // Paper end
         this.quitMessage = quitMessage;
     }
 
@@ -46,4 +53,39 @@ public class PlayerQuitEvent extends PlayerEvent {
     public static HandlerList getHandlerList() {
         return handlers;
     }
+
+    // Paper start
+    @NotNull
+    public QuitReason getReason() {
+        return this.reason;
+    }
+
+    public enum QuitReason {
+        /**
+         * The player left on their own behalf.
+         * <p>
+         * This does not mean they pressed the disconnect button in their client, but rather that the client severed the
+         * connection themselves. This may occur if no keep-alive packet is received on their side, among other things.
+         */
+        DISCONNECTED,
+
+        /**
+         * The player was kicked from the server.
+         */
+        KICKED,
+
+        /**
+         * The player has timed out.
+         */
+        TIMED_OUT,
+
+        /**
+         * The player's connection has entered an erroneous state.
+         * <p>
+         * Reasons for this may include invalid packets, invalid data, and uncaught exceptions in the packet handler,
+         * among others.
+         */
+        ERRONEOUS_STATE,
+    }
+    // Paper end
 }
